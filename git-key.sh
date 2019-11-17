ebb7321e77c6e853bd13516d484b2cb0c7a3b848
HIL <jinma0914@163.com>
2019-11-16 23:59:47 -0800

    key

---
+++ git-key.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+`ssh-keygen -t rsa -C "youremail@example.com"， jinma0914@163.com`;
+`ssh -v git@github.com `;
+`ssh-agent -s`;
+`ssh-add ~/.ssh/id_rsa`;
+`cd ~/.ssh & gedit id_rsa.pub`;
+
