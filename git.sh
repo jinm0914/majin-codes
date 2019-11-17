7b34ca255a330d06c913d99becb489676711babb
HIL <jinma0914@163.com>
2019-11-17 00:03:57 -0800

    "key"

--- git.sh
+++ git.sh
@@ -3,10 +3,11 @@ echo " $0 $1 $2 $# ";
 if [ "-h" = "$1" ]; then  
     echo "上传 :push + \"更新内容\" ||  下载 : pull";
     exit;
+
 elif [ "push" = $1 ]; then 
     if [ -n "$2" ]; then 
         `git add .`;
-        `git commit -m $2`;
+        `git commit -m \"$2\"`;
         `git push -u origin master`;
     else 
         echo "请输入更新内容";    
