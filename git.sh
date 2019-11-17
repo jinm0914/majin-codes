012454eebb1c5a9f56cbdce62b7dca70eaf8517d
HIL <jinma0914@163.com>
2019-11-16 23:41:13 -0800

    test

---
+++ git.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo " $0 $1 $2 $# ";
+if [ "-h" = "$1" ]; then  
+    echo "上传 :push + \"更新内容\" ||  下载 : pull";
+    exit;
+elif [ "push" = $1 ]; then 
+    if [ -n "$2" ]; then 
+        `git add .`;
+        `git commit  -m "$2"`;
+        `git push -u origin master`;
+    else 
+        echo "请输入更新内容";    
+    fi
+
+elif [ "pull" = $1 ];then
+    `git pull`;
+else
+    echo "error !!!"
+fi
+
